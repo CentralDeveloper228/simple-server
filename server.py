fbebcdb88d5b79060b749df0965b68b1bd963ae7
CentralDeveloper228 <98578169+CentralDeveloper228@users.noreply.github.com>
2022-03-05 20:45:52 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+ip = input()
+user_port = input()
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=80)
+    app.run(host=ip, port=user_port)
